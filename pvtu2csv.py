48247c4f1fc365dbe16adf3db3761b7cd4d4e712
w3168 <wis15@ic.ac.uk>
2026-03-26 14:19:05 +1100

    Convert viscosity to csv
    I think during the visualisation step in paraview some of the viscosity
    was converted to negative numbers

--- pvtu2csv.py
+++ pvtu2csv.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 import pyvista as pv
 
-def convert_pvd_to_csv(input_file, output_prefix):
+def convert_pvd_to_csv(input_file, output_prefix, field_name, vector=False):
     """Reads a PVD file and saves data to CSV(s) using PyVista."""
     # Read the PVD file
     reader = pv.PVDReader(input_file)
@@ -16,12 +16,16 @@ def convert_pvd_to_csv(input_file, output_prefix):
         mesh = reader.read()[0]
       
         df = pd.DataFrame(mesh.points, columns=['X','Y', 'Z'])
-        df['displacement_x'] = mesh['displacement'][:, 0]
-        df['displacement_y'] = mesh['displacement'][:, 1]
-        df['displacement_z'] = mesh['displacement'][:, 2]
+
+        if vector:
+            df[f'{field_name}_x'] = mesh[f'{field_name}'][:, 0]
+            df[f'{field_name}_y'] = mesh[f'{field_name}'][:, 1]
+            df[f'{field_name}_z'] = mesh[f'{field_name}'][:, 2]
+        else:
+            df[f'{field_name}'] = mesh[f'{field_name}'][:]
 
         # Construct output filename with time step
-        output_file = f"{output_prefix}_{i}.csv"
+        output_file = f"{output_prefix}_{reader.time_values[i]}.csv"
         df.to_csv(output_file, index=False)
         print(f"Saved: {output_file}")
 
@@ -29,11 +33,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert ParaView .pvd file to .csv using PyVista")
     parser.add_argument("-i", "--input", required=True, help="Input .pvd file")
     parser.add_argument("-o", "--output", default="output", help="Prefix for output .csv files")
+    parser.add_argument("--field_name", default="displacement", help="Name of field to extract")
+    parser.add_argument("--vector", action='store_true', help="Specify if field is vector valued")
     
     args = parser.parse_args()
     
     if not os.path.exists(args.input):
         print(f"Error: File {args.input} not found.")
     else:
-        convert_pvd_to_csv(args.input, args.output)
-
+        convert_pvd_to_csv(args.input, args.output, args.field_name, args.vector)
